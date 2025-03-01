53edec2fd07b01ec1793e70deec6457f7187114c
sarah-25133 <hydeist_rock@yahoo.co.jp>
2025-03-01 19:59:49 +0900

    kadai_010をアップロード

---
+++ kadai_010/holiday.js
@@ -0,0 +1,14 @@
+const holidays = ["元日", "成人の日", "建国記念の日", "天皇誕生日", "春分の日", "昭和の日", "憲法記念日", "みどりの日", "こどもの日", "海の日", "山の日", "敬老の日", "秋分の日", "スポーツの日", "文化の日", "勤労感謝の日"]
+
+
+//while文で繰り返し処理を行う
+var count = 0;
+while (count < 1) {
+  console.log(holidays);
+  count++;
+}
+
+//for文で繰り返し処理を行う
+for (var i = 0; i < 1; i++) {
+  console.log(holidays);
+}
